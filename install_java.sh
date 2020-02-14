39d018a71d67ee2cecf5df82aed9faa11fa59965
michael-stappers <58463286+michael-stappers@users.noreply.github.com>
2020-02-14 15:43:20 +0100

    fix commmented operation

--- install_java.sh
+++ install_java.sh
@@ -1,8 +1,8 @@
 # Check argument value
 if [[ $1 == 8 || $1 == 11 ]]; then
     # Installation
-    #sudo apt update
-    #sudo apt install openjdk-$1-jdk
+    sudo apt update
+    sudo apt install openjdk-$1-jdk
 
     # Export the var and update the path for the steps after this action
     export JAVA_HOME=/usr/lib/jvm/java-$1-openjdk-amd64
