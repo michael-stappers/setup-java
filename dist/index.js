4db705cbc3ee757f7064d69835d9d3da1631dc94
michael-stappers <58463286+michael-stappers@users.noreply.github.com>
2020-02-19 11:17:38 +0100

    fix typo

--- dist/index.js
+++ dist/index.js
@@ -958,9 +958,9 @@ async function run() {
     console.log(`src: ${src}`);
     console.log('Install tree');
     await exec.exec('sudo apt install tree');
-    await exec.exec('ls -la ../../_action');
+    await exec.exec('ls -la ../../_actions');
     console.log('LS ../../..');
-    await exec.exec('tree ../../_action');
+    await exec.exec('tree ../../_actions');
   } 
   catch (error) {
     core.setFailed(error.message);
